cad7987baf061e7b3474cf540e7f8dbe181571bc
Xuanwo <github@xuanwo.io>
2021-08-24 11:49:21 +0800

    feat: Add all stable service support
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- cmd/beyondctl/services.go
+++ cmd/beyondctl/services.go
@@ -1,6 +1,17 @@
 package main
 
 import (
+	_ "github.com/beyondstorage/go-service-azblob/v2"
+	_ "github.com/beyondstorage/go-service-cos/v2"
+	_ "github.com/beyondstorage/go-service-dropbox/v2"
 	_ "github.com/beyondstorage/go-service-fs/v3"
+	_ "github.com/beyondstorage/go-service-ftp"
+	_ "github.com/beyondstorage/go-service-gcs/v2"
+	_ "github.com/beyondstorage/go-service-ipfs"
+	_ "github.com/beyondstorage/go-service-kodo/v2"
+	_ "github.com/beyondstorage/go-service-memory"
+	_ "github.com/beyondstorage/go-service-minio"
+	_ "github.com/beyondstorage/go-service-oss/v2"
 	_ "github.com/beyondstorage/go-service-s3/v2"
+	_ "github.com/beyondstorage/go-service-uss/v2"
 )
