1685210423c56dbc3428e847792f1e72dbf750f7
Prnyself <281239768@qq.com>
2021-08-24 18:02:14 +0800

    Implement BCP-21: Parse Profile Input

--- config/profile.go
+++ config/profile.go
@@ -1,6 +1,13 @@
 package config
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const profileSeparator = ":"
 
 type Profile struct {
 	Connection string `json:"connection" toml:"connection"`
@@ -24,4 +31,51 @@ func (c *Config) RemoveProfile(name string) {
 	defer c.Unlock()
 
 	delete(c.Profiles, name)
+}
+
+func (c *Config) ParseProfileInput(input string) (conn, key string, err error) {
+	c.Lock()
+	defer c.Unlock()
+
+	sepIdx := strings.LastIndex(input, profileSeparator)
+
+	// separator not found, treat as normal fs path
+	// 1. if path is absolute path, set work dir is /
+	// 2. if path is relative path, set work dir is current dir
+	// in both condition, treat path as object key
+	if sepIdx == -1 {
+		prefix := "fs://"
+		// absolute path
+		if strings.HasPrefix(input, "/") {
+			conn = prefix + "/"
+		} else {
+			curDir, err := os.Getwd()
+			if err != nil {
+				return "", "", err
+			}
+			conn = prefix + curDir
+		}
+
+		key = input
+		return
+	}
+
+	// handle profile
+	name := input[:sepIdx]
+	prof, ok := c.Profiles[name]
+	// profile not exist, treat as full connection string
+	if !ok {
+		return "", "", fmt.Errorf("profile with name %s not exist", name)
+	}
+
+	conn = prof.Connection
+
+	// handle key
+	// if input end with ':', set key to blank
+	if sepIdx == len(input)-1 {
+		key = ""
+	} else {
+		key = input[sepIdx+1:]
+	}
+	return
 }
